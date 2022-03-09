2bab4a2a4e8a5c66330fc28da6662772d7bb7e87
three97-dev <vitalii.nobis@neatbyte.solutions>
2022-03-09 22:16:55 +0000

    Fix bug with long texts

--- src/components/large-tile/LargeTile.js
+++ src/components/large-tile/LargeTile.js
@@ -5,19 +5,21 @@ import Image from "../basic/image/Image";
 
 import UniversalLink from "../../utils/UniversalLink";
 
+import "./LargeTile.css";
+
 const LargeTile = ({ link, image, title, description }) => {
   return (
     <div className="px-13px lg:px-0px pb-15px">
       <div className="w-250px lg:w-349px filter drop-shadow-tile rounded-15px overflow-hidden mx-auto">
         <UniversalLink link={link}>
-          <Image image={image} className="w-250px lg:w-349px h-218px lg:h-349px"/>
+          <Image image={image} className="w-250px lg:w-349px h-218px lg:h-349px" />
           <div className="text-center p-20px bg-white">
             <h3 className="text-black mb-10px lg:mb-20px">{title}</h3>
-            <p className="text-black-gray">{description}</p>
+            <p className="text-black-gray h-124px lg:h-104px w-210px lg:w-310px large-tile-text-cut">{description}</p>
           </div>
         </UniversalLink>
       </div>
- </div>
+    </div>
   );
 };
 
