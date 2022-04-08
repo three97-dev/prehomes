33c0c0b0fc89eb44b00cc8a1cfade42c925b4191
three97-dev <vitalii.nobis@neatbyte.solutions>
2022-04-08 20:46:16 +0000

    Fix URL build issue (add fallback to id)

--- src/utils/buildUrl.js
+++ src/utils/buildUrl.js
@@ -1,20 +1,25 @@
 const slugify = require("slugify");
 
-const strToUrl = str => {
-  return slugify(str, { lower: true, remove: /[*+~.()'"!:@]/g });
+const strToUrl = (str, fallback) => {
+  let slug = slugify(str, { lower: true, remove: /[*+~.()'"!:@]/g, strict: true });
+  if (slug === "") {
+    return fallback;
+  }
+  return slug;
 };
 
 exports.buildProjectUrl = project => {
-  return `/projects/${strToUrl(project.projectName)}`;
+  return `/projects/${strToUrl(project.projectName, project.contentful_id)}`;
 };
 
 exports.buildCityUrl = city => {
-  return `/cities/${strToUrl(city.cityName)}`;
+  return `/cities/${strToUrl(city.cityName, city.contentful_id)}`;
 };
 
 exports.buildDeveloperUrl = developer => {
-  return `/developers/${strToUrl(developer.developerName)}`;
+  return `/developers/${strToUrl(developer.developerName, developer.contentful_id)}`;
 };
+
 exports.buildProjectTypeUrl = projectType => {
-  return `/project-type/${strToUrl(projectType.name)}`;
+  return `/project-type/${strToUrl(projectType.name, projectType.contentful_id)}`;
 };
